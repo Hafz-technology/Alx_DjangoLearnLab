5114038c04ce9e578fc92fcf4b22f41dce700778
Khalil Hafz <129151938+Hafz-technology@users.noreply.github.com>
2025-09-07 19:45:17 +0300

    task1

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,8 @@
+# relationship_app/urls.py
+from django.urls import path
+from .views import book_list_view, LibraryDetailView
+
+urlpatterns = [
+    path('books/', book_list_view, name='book-list'),
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
 
 # Create your views here.
+
+# Function-based view to list all books
+def book_list_view(request):
+    books = Book.objects.all().select_related('author')
+    context = {'books': books}
+    return render(request, 'relationship_app/list_books.html', context)
+
+# Class-based view to display details of a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Ensure that books are prefetched to avoid N+1 queries
+        context['library'].books.all().prefetch_related('author')
+        return context
