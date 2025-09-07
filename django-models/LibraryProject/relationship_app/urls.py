4d7995adb8faa6946155adffdf1cd9bdc0bbd499
Khalil Hafz <129151938+Hafz-technology@users.noreply.github.com>
2025-09-07 19:48:57 +0300

    task1

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 # relationship_app/urls.py
 from django.urls import path
 from .views import book_list_view, LibraryDetailView
+from .views import list_books
 
 urlpatterns = [
     path('books/', book_list_view, name='book-list'),
