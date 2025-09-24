d8ef4a54cad3cdebc2cba4eb41f5a4850f8c341a
Hafz-technology <khalilhafz@gmail.com>
2025-09-24 20:51:26 +0300

    task1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import  Book
 # Create your views here.
 
 def index(request):
     return HttpResponse("Welcome to the Relationship App!")
+
+
+def list_books(request):
+    books = Book.objects.all()
+    context = {'list_books': books}
+    return render(request, 'books/list_books.html', context)
+
+
+
+
