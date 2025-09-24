36fe9c09c56c614710c610ffc12c8fdb03ec290e
Hafz-technology <khalilhafz@gmail.com>
2025-09-24 20:59:28 +0300

    task1

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import  Author, Book, Library, Librarian
+
 # Register your models here.
 
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,7 +10,7 @@ def index(request):
 def list_books(request):
     books = Book.objects.all()
     context = {'list_books': books}
-    return render(request, 'books/list_books.html', context)
+    return render(request, '../relationship_app/list_books.html', context)
 
 
 
