f18c9b1a3dc6f0ae350ce7861d79afd69839c501
Hafz-technology <khalilhafz@gmail.com>
2025-09-24 21:48:00 +0300

    task1

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .views import LibraryDetailView
 
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('', views.index, name='index'),
     
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import  Book
+from .models import Author, Book, Library, Librarian
+from django.views.generic import DetailView
 # Create your views here.
 
 def index(request):
@@ -10,8 +11,18 @@ def index(request):
 def list_books(request):
     books = Book.objects.all()
     context = {'list_books': books}
-    return render(request, '../relationship_app/list_books.html', context)
+    return render(request, '../templates/relationship_app/list_books.html', context)
 
 
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = '../templates/relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add the books related to this specific library to the context
+        context['books'] = self.object.books.all()
+        return context
 
 
