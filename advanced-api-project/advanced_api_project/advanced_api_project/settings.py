3cff6b8d1b87dc69931cc067b113e3f7a0aba7ba
Hafz-technology <khalilhafz@gmail.com>
2025-09-30 09:53:30 +0300

    Advanced API Development with Django REST Framework task0

--- advanced-api-project/advanced_api_project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/advanced_api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-69n=w^%vi#1rz3tfc9rgru9+#5skuw^2#oq#!^chu!55-&*a#8'
+SECRET_KEY = 'django-insecure-8l%2c7^plolvu&x-r!h6c^%o6(o0-y^*2x#o_&mf9fwb-d%@8w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'api',
 ]
 
 MIDDLEWARE = [
