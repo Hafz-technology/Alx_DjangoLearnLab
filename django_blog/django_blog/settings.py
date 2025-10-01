aa6a734d946fef679332dd4c09ef88b8eeb1a23a
Hafz-technology <khalilhafz@gmail.com>
2025-10-01 12:48:28 +0300

    Building a Complete Django Application task0

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -73,25 +73,25 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'my_blog_db',         
-        'USER': 'postgres',         
-        'PASSWORD': 'my_db_password', 
-        'HOST': 'localhost',          
-        'PORT': '5432',               
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#         # ["USER", "PORT"]
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'my_blog_db',         
+#         'USER': 'postgres',         
+#         'PASSWORD': 'my_db_password', 
+#         'HOST': 'localhost',          
+#         'PORT': '5432',               
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # ["USER", "PORT"]
+    }
+}
 
 
 # Password validation
