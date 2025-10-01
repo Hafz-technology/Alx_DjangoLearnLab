683388ff36fb969c19f7a0b65fb3a27b6b2e4f93
Hafz-technology <khalilhafz@gmail.com>
2025-10-01 11:51:01 +0300

    Building a Complete Django Application task0

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,16 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Book(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='blog_posts')
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        # Optional: Order posts by published_date in descending order (newest first)
+        ordering = ['-published_date']
 
-
-# Model Specifications:
-# Create a model Post in blog/models.py with the following fields:
-# title: models.CharField(max_length=200)
-# content: models.TextField()
-# published_date: models.DateTimeField(auto_now_add=True)
-# author: models.ForeignKey to Django’s User model, with a relation to handle multiple posts by a single author.
-# Run the migrations to create the model in the database: bash python manage.py makemigrations blog python manage.py migrate
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -52,10 +52,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'django_blog.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +116,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
