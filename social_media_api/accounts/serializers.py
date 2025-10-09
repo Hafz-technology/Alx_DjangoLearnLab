2a31b1d894e6717b3f133cef267b1e6e93f7dbfb
Hafz-technology <khalilhafz@gmail.com>
2025-10-09 15:44:39 +0300

    Building and deploying a Django API task0

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import User
+from rest_framework.authtoken.models import Token
+
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
