e7d660b208d153a88059bff5ac22c1c832254653
Hafz-technology <khalilhafz@gmail.com>
2025-10-11 11:21:10 +0300

    Building and deploying a Django API task2

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CreateUserView, RetrieveUpdateProfileView, CustomAuthToken
+from .views import CreateUserView, RetrieveUpdateProfileView, CustomAuthToken, FollowUserView
 
 
 # Accounts End Points
@@ -7,4 +7,8 @@ urlpatterns = [
     path('register/', CreateUserView.as_view(), name='register'),
     path('login/', CustomAuthToken.as_view(), name='login'),
     path('profile/', RetrieveUpdateProfileView.as_view(), name='profile'),
+    path('follow/<int:pk>/', FollowUserView.as_view(), name='follow-user'),
 ]
+
+
+
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -3,6 +3,7 @@ from rest_framework.filters import SearchFilter # Import SearchFilter
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
+from rest_framework import generics
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at') # Order by newest first
@@ -24,5 +25,21 @@ class CommentViewSet(viewsets.ModelViewSet):
         serializer.save(author=self.request.user)
 
 
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the posts
+        by users that the currently authenticated user follows.
+        """
+        current_user = self.request.user
+        followed_users = current_user.following.all()
         
-        
+        # Filter posts to only include those from followed users
+        # and order them by the most recent
+        return Post.objects.filter(author__in=followed_users).order_by('-created_at')     
+    
+    
+    
